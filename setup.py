ab0b0c4dac8e0a8e24bfdc859535d75f01fea213
Parakoopa <parakoopa@live.de>
2019-04-07 23:12:02 +0200

    Changes to setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,19 @@ setup(
     name='riptide_proxy',
     version='0.1',
     packages=find_packages(),
-    include_package_data=True,
+    description='TODO',  # TODO
+    long_description='TODO',  # TODO
     install_requires=[
+        'riptide_lib == 0.1',
         'tornado >= 5.1',
         'Click >= 7.0',
-        # todo
+        'recordclass >= 0.7',
+        'python-prctl >= 1.7; sys_platform == "linux"',
+        'certauth >= 1.2',
+    ],
+    # TODO
+    classifiers=[
+        'Programming Language :: Python',
     ],
     entry_points='''
         [console_scripts]
