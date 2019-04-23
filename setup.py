0ee10720c55dab5651a10154d3257b4788495b03
Parakoopa <parakoopa@live.de>
2019-04-23 18:01:38 +0200

    setup.py: Making certauth and prctl 'optional'.

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
 from setuptools import setup, find_packages
+import sys
+import subprocess
+from setuptools.command.install import install
 
 # README read-in
 from os import path
@@ -6,6 +9,36 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 # END README read-in
+
+
+# Additional packages, that rely on system features in order to be installable.
+# Don't fail on errors, but tell the user that installation failed.
+# This is mainly to be able to install it on Read the Docs, and is otherwise not supported.
+ADDITIONAL_REQS_PRCTL = 'python-prctl >= 1.7; sys_platform == "linux"'
+ADDITIONAL_REQS_CERTAUTH = 'certauth >= 1.2'
+
+
+class AdditionalRequirements(install):
+    def run(self):
+        # LINUX: prctl
+        print('-------------')
+        print('riptide-proxy: Installing additional requirement: ' + ADDITIONAL_REQS_PRCTL)
+        retcode = subprocess.call([sys.executable, "-m", "pip", "install", ADDITIONAL_REQS_PRCTL])
+        if retcode != 0:
+            print('!!! WARNING: Could not install python-prctl. Running with sudo impossible, the proxy may or may '
+                  'not work correctly! Please install prctl-dev and reinstall the proxy server.')
+
+        # ALL PLATFORMS: cerauth
+        print('-------------')
+        print('riptide-proxy: Installing additional requirement: ' + ADDITIONAL_REQS_CERTAUTH)
+        retcode = subprocess.call([sys.executable, "-m", "pip", "install", ADDITIONAL_REQS_CERTAUTH])
+        if retcode != 0:
+            print('!!! WARNING: Could not install certauth. HTTPS impossible to use, the proxy may or may '
+                  'not work correctly! Please fix OpenSSL setup and reinstall the proxy server.')
+        print('-------------')
+
+        install.run(self)
+
 
 setup(
     name='riptide-proxy',
@@ -20,10 +53,16 @@ setup(
         'riptide-lib >= 0.1, < 0.2',
         'tornado >= 5.1',
         'Click >= 7.0',
-        'recordclass >= 0.7',
-        'python-prctl >= 1.7; sys_platform == "linux"',
-        'certauth >= 1.2',
+        'recordclass >= 0.7'
     ],
+    # The additional requirements are also in extras_require to be parsable by tools.
+    extras_require={'extras': [
+        ADDITIONAL_REQS_CERTAUTH,
+        ADDITIONAL_REQS_PRCTL
+    ]},
+    cmdclass={
+        'install': AdditionalRequirements,
+    },
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Programming Language :: Python',
