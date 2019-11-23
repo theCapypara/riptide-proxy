978a193b0916a9ddaa42730c76d951631ce47cfa
Parakoopa <parakoopa@live.de>
2019-11-23 02:02:52 +0100

    Support to start Riptide Mission Control

--- riptide_proxy/server/starter.py
+++ riptide_proxy/server/starter.py
@@ -1,11 +1,13 @@
 import logging
-import pkg_resources
 import tornado.httpserver
 import tornado.ioloop
 import tornado.routing
 import tornado.web
+from importlib.util import find_spec
 
+from riptide.config.document.config import Config
 from riptide.config.loader import load_projects
+from riptide.engine.abstract import AbstractEngine
 from riptide_proxy import LOGGER_NAME
 from riptide_proxy.project_loader import RuntimeStorage
 from riptide_proxy.resources import get_resources
@@ -14,17 +16,47 @@ from riptide_proxy.server.websocket.others import ProxyWebsocketHandler
 from riptide_proxy.server.websocket.autostart import AutostartHandler
 
 logger = logging.getLogger(LOGGER_NAME)
+RIPTIDE_MISSION_CONTROL_SUBDOMAIN = "control"
 
 
-def run_proxy(system_config, engine, http_port, https_port, ssl_options, start_ioloop=True):
+def load_plugin_routes(system_config: Config, engine: AbstractEngine, https_port):
+    # Riptide Mission Control
+    mc_spec = find_spec("riptide_mission_control")
+    routes = []
+    if mc_spec is not None:
+        from riptide_mission_control.server.starter import get_for_external
+
+        start_https_msg = ""
+
+        if https_port:
+            start_https_msg = f"\n    https://{RIPTIDE_MISSION_CONTROL_SUBDOMAIN}.{system_config['proxy']['url']}:{system_config['proxy']['ports']['https']:d}"
+
+        logger.info(
+            f"Riptide Mission Control is also started at:\n"
+            f"    http://{RIPTIDE_MISSION_CONTROL_SUBDOMAIN}.{system_config['proxy']['url']}:{system_config['proxy']['ports']['http']:d}{start_https_msg}"
+        )
+        routes += get_for_external(
+            system_config,
+            engine,
+            f"{RIPTIDE_MISSION_CONTROL_SUBDOMAIN}.{system_config['proxy']['url']}"
+        )
+    return routes
+
+
+def run_proxy(system_config: Config, engine: AbstractEngine, http_port, https_port, ssl_options, start_ioloop=True):
     """
     Run proxy on the specified port. If start_ioloop is True (default),
     the tornado IOLoop will be started immediately.
     """
 
+    start_https_msg = ""
+
+    if https_port:
+        start_https_msg = f"\n    https://{system_config['proxy']['url']}:{system_config['proxy']['ports']['https']:d}"
+
     logger.info(
-        f"Starting Riptide Proxy on HTTP: "
-        f"http://{system_config['proxy']['url']}:{system_config['proxy']['ports']['http']:d}"
+        f"Starting Riptide Proxy at: \n"
+        f"    http://{system_config['proxy']['url']}:{system_config['proxy']['ports']['http']:d}{start_https_msg}"
     )
 
     # Load projects initially
@@ -38,7 +70,7 @@ def run_proxy(system_config, engine, http_port, https_port, ssl_options, start_i
     }
 
     # Configure Routes
-    app = tornado.web.Application([
+    app = tornado.web.Application(load_plugin_routes(system_config, engine, https_port) + [
         # http
         (RiptideNoWebSocketMatcher(r'^(?!/___riptide_proxy_ws).*$'), ProxyHttpHandler, storage),
         # Any non-autostart websockets
@@ -52,10 +84,6 @@ def run_proxy(system_config, engine, http_port, https_port, ssl_options, start_i
 
     # Prepare HTTPS
     if https_port:
-        logger.info(
-            f"Starting Riptide Proxy on HTTPS: "
-            f"https://{system_config['proxy']['url']}:{system_config['proxy']['ports']['https']:d}"
-        )
         https_app = tornado.httpserver.HTTPServer(app, ssl_options=ssl_options, xheaders=True)
         https_app.listen(https_port)
 
