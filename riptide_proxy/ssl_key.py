512b902ad741234f5a85b89fcf8e88af8cefc08c
Marco Köpcke <marco.koepcke@tudock.de>
2019-11-29 11:47:39 +0100

    Fixes SSL key generation on MacOS Catalina, by reducing Not Valid After date. Version bump to 0.3.4

--- riptide_proxy/ssl_key.py
+++ riptide_proxy/ssl_key.py
@@ -1,13 +1,18 @@
 """This module manages HTTPS for the server"""
-
+import logging
 import os
 import socket
 from certauth.certauth import CertificateAuthority
 
 from riptide.config.files import riptide_config_dir
+from riptide_proxy import LOGGER_NAME
 
 RIPTIDE_PROXY_CONFIG_DIR = 'riptide_proxy'
 CA_NAME = 'ca.pem'
+
+NOT_VALID_AFTER = 364 * 24 * 60 * 60
+
+logger = logging.getLogger(LOGGER_NAME)
 
 
 def get_config_dir():
@@ -23,5 +28,16 @@ def get_ca_path():
 def create_keys(temp_dir, common_name):
     """Create wildcard certificate for the proxy base url."""
     ca = CertificateAuthority(f'Riptide Proxy CA for {socket.gethostname()}',
-                              get_ca_path(), cert_cache=temp_dir)
+                              get_ca_path(), cert_cache=temp_dir,
+                              # Make it valid for 364 days, as per MacOS Catalina requirements
+                              cert_not_after=NOT_VALID_AFTER)
+
+    if ca.ca_cert.has_expired():
+        logger.warning("The CA certificate had to be re-generated because"
+                       "it was no longer valid. You may need to re-import it,"
+                       "please see the documentation.")
+        ca = CertificateAuthority(f'Riptide Proxy CA for {socket.gethostname()}',
+                              get_ca_path(), cert_cache=temp_dir,
+                              cert_not_after=NOT_VALID_AFTER, overwrite=True)
+
     return ca.get_wildcard_cert('*.' + common_name)
